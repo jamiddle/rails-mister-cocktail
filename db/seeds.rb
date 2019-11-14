b0c8e14f4ec6016ba8925000df1c796413a7d876
James Middlemass <james.middlemass01@gmail.com>
2019-11-14 15:02:58 +0000

    halfway through making cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cocktail.create(name: 'Sex on the Beach')
+Cocktail.create(name: 'Nice Cocktail')
+Cocktail.create(name: 'Juicy Lucy')
